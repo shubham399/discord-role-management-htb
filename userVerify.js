d44e247e251021c1e1dfe9255ca6a0d7098d990f
Shubham <shubham.399@gmail.com>
2019-07-07 21:38:38 +0530

    cleanup of code.

--- userVerify.js
+++ userVerify.js
@@ -1,60 +1,65 @@
 const logger = require("./log.js").logger
-const request = require("request");
-const constant = require("./constant.js")
+const axios = require("axios");
+const R = require('ramda');
+const constant = require("./constant.js");
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
 const profilePostChannel = process.env.PROFILE_CHANNEL;
 const assignRole = process.env.ASSIGN_ROLE;
 
-function verifyUser(msg, token) {
+const getUserData = (token) => {
+  logger.debug(token)
+  logger.debug(constant.htburl + token)
+  return axios.get(constant.htburl + token)
+};
+
+const verifyUser = async (function(msg, token) {
   try {
-    let author = msg.author
-    let member = msg.member
-    let channel = msg.channel
-    request(constant.htburl + token, {
-      json: true
-    }, (err, res, body) => {
-      if (err) {
-        logger.error(err);
+    let author = msg.author;
+    let member = msg.member;
+    let channel = msg.channel;
+    let result = null;
+    try {
+      result = await (getUserData(token)).data;
+    } catch (error) {
+      logger.error("Axios Error:" + error);
+      logger.error(error);
+      if (R.path(["response", "status"], error) === 404) {
+        channel.send(constant.invalidToken(author))
+      } else {
         channel.send(constant.htbFailure(author))
       }
-      logger.debug(body);
-      var rank = null;
-      try {
-        rank = body.rank.replace(/ /g, '')
-      } catch (err) {
-        logger.error("Rank: " + err);
-        channel.send(constant.invalidToken(author))
-      }
-      if (rank != null) {
-        try{
-        var htbprofile = msg.guild.channels.find(channel => channel.name === profilePostChannel)
-        var defaultRole = member.guild.roles.find(r => r.name === assignRole );
-        var hasRole = member.roles.find(role => {
-          return role.name == defaultRole.name
-        });
-        }catch(error){
-           msg.author.send(constant.dmFailure);
-           logger.error("Error:" + error);
-           console.error( error);
-        }
-        logger.debug("HasRole: " + (hasRole != null ? hasRole.name:null))
-        if (hasRole == null) {
-          if (defaultRole != null){
-           logger.info(author.username + " htb rank is " + rank + " and giving it role " + defaultRole.name);
-            member.addRoles([defaultRole]).then(r => {
-              htbprofile.send(constant.profile(author,body.user_id)).catch(err => console.error(err))
-              channel.send(constant.success(author))
-            }).catch((e)=>{
-              logger.error("Error:" +e);
-              channel.send(constant.unableToaddRole(author))});
-        }
-        } else {
-          logger.info(author.username + " already have the role.");
-          channel.send(constant.alreadyVerified(author))
-        }
-      }
-    });
+    }
+    let rank = result.rank;
+    let htbprofile = msg.guild.channels.find(channel => channel.name === profilePostChannel)
+    let defaultRole = member.guild.roles.find(r => r.name === assignRole);
+    let hasRole = member.roles.find(role => role.name == defaultRole.name);
+    logger.debug("API Respone: " + JSON.stringify(result));
+    logger.debug("HasRole: " + (hasRole != null ? hasRole.name : null))
+    logger.debug(rank);
+    if (!hasRole) {
+      logger.info(author.username + " htb rank is " + rank + " and giving it role " + defaultRole.name);
+      member.addRoles([defaultRole]).then(r => {
+        htbprofile.send(constant.profile(author, result.user_id)).catch(err => console.error(err))
+        channel.send(constant.success(author))
+      }).catch((e) => {
+        logger.error("Error:" + e);
+        channel.send(constant.unableToaddRole(author))
+      });
+    } else {
+      logger.verbose(author.username + " already have the role.");
+      await (channel.send(constant.alreadyVerified(author)));
+    }
   } catch (error) {
-    logger.error("Error:" + error);
+    logger.error("adding verify:" + error);
+    msg.author.send(constant.dmFailure("#bot-spam"));
+  } finally {
+    try {
+      await (msg.delete());
+    } catch (error) {
+      logger.error("Message Delete:" + error);
+    }
   }
-}
+});
+
 module.exports.verifyUser = verifyUser;
