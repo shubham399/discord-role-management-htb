d9894da7a9a98f4cb047311dd0baae39ed919444
Shubham <shubham.399@gmail.com>
2019-07-07 21:47:03 +0530

    code formating.

--- log.js
+++ log.js
@@ -1,16 +1,29 @@
 const winston = require('winston');
-const level = process.env.LOG_LEVEL || "debug";
-const { combine, timestamp, label, printf } = winston.format;
+const level = process.env.LOG_LEVEL || "verbose";
+const os = require("os")
+const {
+  combine,
+  timestamp,
+  label,
+  printf
+} = winston.format;
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
+const myFormat = printf(({
+  level,
+  message,
+  label,
+  timestamp
+}) => {
   if (typeof message)
-  message = JSON.stringify(message);
-  return `${timestamp} [${label}] ${level}: ${message}`;
+    message = JSON.stringify(message);
+  return `${timestamp} ${label} ${level}: ${message}`;
 });
 module.exports.logger = winston.createLogger({
   level: level,
   format: combine(
-    label({ label: 'Bot! ' }),
+    label({
+      label: os.hostname()
+    }),
     timestamp(),
     myFormat
   ),
@@ -18,7 +31,9 @@ module.exports.logger = winston.createLogger({
     new winston.transports.Console({
       format: combine(
         winston.format.colorize(),
-        label({ label: 'Bot! ' }),
+        label({
+          label: os.hostname()
+        }),
         timestamp(),
         myFormat
       )
