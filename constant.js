d388687bb35cdd7e06c6dbce0926b5ce0d6eec85
Shubham <shubham.399@gmail.com>
2019-07-07 21:39:00 +0530

    channel.

--- constant.js
+++ constant.js
@@ -3,7 +3,7 @@ exports.default = (bot) =>`type \`\`${bot} help\`\` to see how to use`
 exports.help = (bot,channel) =>`Hey Lets Get you verified, Go grab the identification token from HTB from https://hackthebox.eu/home/settings and paste \`${bot} verify <token>\` in ${channel}`
 exports.unkown = "Invalid! Command, Please try with a proper command"
 exports.htburl = 'https://www.hackthebox.eu/api/users/identifier/'
-exports.dmFailure = "Send the badger verify in the channel"
+exports.dmFailure = (channel) => `Send the badger verify in the ${channel}`
 exports.htbFailure = (author) => `Unable to connect to HTB server ${author}`
 exports.invalidToken = (author) => `Invalid token ${author} Please try with the proper one.`
 exports.profile = (author,userid) => `${author} HTB Profile: https://www.hackthebox.eu/home/users/profile/${userid}`
