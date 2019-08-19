e25cca38d93106f29d7af1e7916a570f6ccbf570
Shubham <shubham.399@gmail.com>
2019-08-19 19:37:02 +0530

    updated Remind code.

--- commands/remind.js
+++ commands/remind.js
@@ -9,17 +9,23 @@ const profilePostChannel = process.env.PROFILE_CHANNEL;
 const assignRole = process.env.ASSIGN_ROLE;
 const actionLog = process.env.ACTION_LOG || "action-log";
 const botTriggerCommand = process.env.BOT_TRIGGER_COMMAND;
-const sendHelp = require("./help").sendHelp
+const sendHelp = require("./help").sendHelp;
+const ignoreList = process.env.REMIND_IGNORE_LIST;
+const guildId = process.env.GUILD_ID;
 
 module.exports.run = async (bot, message, args) => {
   message.delete(2000);
-  if (!message.member.hasPermission(["BAN_MEMBERS"])) return message.channel.send("You do not have permission to perform this command!")
+  if (!message.member.hasPermission(["ADMINISTRATOR"])) return message.channel.send("You do not have permission to perform this command!")
   try {
-    let unVerifedMembers = message.guild.members.filter(member => !member.user.bot).filter((member, result) => {
+    let ignoreListArray = ignoreList.trim().split(",")
+    let guild = await(bot.guilds.array().find(x => x.id === guildId).fetchMembers())
+    let unVerifedMembers = guild.members.filter(member => !member.user.bot).filter(member => !ignoreListArray.includes(member.user.username)).filter((member, result) => {
       let defaultRole = member.guild.roles.find(r => r.name === assignRole);
       let hasRole = member.roles.find(role => role.name == defaultRole.name);
       return !hasRole
-    }).map(async (member => {
+    })
+    logger.info("Sending Reminders to: "+unVerifedMembers.size + " members")
+    unVerifedMembers.map(async (member => {
       try {
         logger.verbose("Reminding: " + member.displayName);
         await (member.send("This is a gentle reminder to verify yourself on this server."));
