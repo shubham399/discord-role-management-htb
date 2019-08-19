d5829856e80cd61662405d2a1483f99a3672d1e4
Shubham <shubham.399@gmail.com>
2019-08-19 16:39:52 +0530

    cleanup.

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -11,45 +11,43 @@ const botTriggerCommand = process.env.BOT_TRIGGER_COMMAND;
 const listRole = process.env.LIST_ROLE || "STAFF";
 
 module.exports.run = async (bot, message, args) => {
-  try{
-  let embed = new Discord.RichEmbed()
-    .setColor("#5780cd")
-    .setTitle("Server Info")
-    .setThumbnail(message.guild.iconURL)
-    .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
-    .addField("**Guild Name:**", `${message.guild.name}`, true)
-    .addField("**Guild Owner:**", `${message.guild.owner}`, true)
-    .addField("**Member Count:**", `${message.guild.memberCount}`, true)
-    .addField("**Verified Member Count:**", `${message.guild.members.filter(member =>{
+  try {
+    let embed = new Discord.RichEmbed()
+      .setColor("#5780cd")
+      .setTitle("Server Info")
+      .setThumbnail(message.guild.iconURL)
+      .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
+      .addField("**Guild Name:**", `${message.guild.name}`, true)
+      .addField("**Guild Owner:**", `${message.guild.owner}`, true)
+      .addField("**Member Count:**", `${message.guild.memberCount}`, true)
+      .addField("**Verified Member Count:**", `${message.guild.members.filter(member =>{
       let defaultRole = member.guild.roles.find(r => r.name === assignRole);
       let hasRole = member.roles.find(role => role.name == defaultRole.name);
       return hasRole
     }).size}`, true)
-    .addField("**Channels Count:**", `${message.guild.channels.size}`, true)
-    .addField("**Role Count:**", `${message.guild.roles.size}`, true)
-    .addField("**Role List:**", `${message.guild.roles.map(role=>role.name)}`, true)
-    .setFooter(`${botTriggerCommand}`, bot.user.displayAvatarURL);
-  await (message.channel.send({
-    embed
-  }));
-  let verifedMemberList = new Discord.RichEmbed()
-    .setColor("#5780cd")
-    .setTitle(`${listRole} List`)
-    .setThumbnail(message.guild.iconURL)
-    .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
-    .addField(`**${listRole} List:**`, `${message.guild.members.filter((member,result) =>{
+      .addField("**Channels Count:**", `${message.guild.channels.size}`, true)
+      .addField("**Role Count:**", `${message.guild.roles.size}`, true)
+      .addField("**Role List:**", `${message.guild.roles.map(role=>role.name)}`, true)
+      .setFooter(`${botTriggerCommand}`, bot.user.displayAvatarURL);
+    await (message.channel.send({
+      embed
+    }));
+    let verifedMemberList = new Discord.RichEmbed()
+      .setColor("#5780cd")
+      .setTitle(`${listRole} List`)
+      .setThumbnail(message.guild.iconURL)
+      .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
+      .addField(`**${listRole} List:**`, `${message.guild.members.filter((member,result) =>{
       let defaultRole = member.guild.roles.find(r => r.name === listRole);
       let hasRole = member.roles.find(role => role.name == defaultRole.name);
       return hasRole
       }).map(member => member.user.username)}`, true)
-  await (message.channel.send({
-    verifedMemberList
-  }));
-}
-catch(err)
-{
-  logger.error(err);
-}
+    await (message.channel.send({
+      verifedMemberList
+    }));
+  } catch (err) {
+    logger.error(err);
+  }
 }
 
 module.exports.config = {
