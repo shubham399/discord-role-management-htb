01a43d6e8ab7ecb877684f61e5c3d3a3626987cc
Shubham <shubham.399@gmail.com>
2019-08-19 16:35:11 +0530

    Send Verifed Member List.

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -11,7 +11,7 @@ const botTriggerCommand = process.env.BOT_TRIGGER_COMMAND;
 
 
 module.exports.run = async (bot, message, args) => {
-    let embed = new Discord.RichEmbed()
+  let embed = new Discord.RichEmbed()
     .setColor("#5780cd")
     .setTitle("Server Info")
     .setThumbnail(message.guild.iconURL)
@@ -24,20 +24,30 @@ module.exports.run = async (bot, message, args) => {
       let hasRole = member.roles.find(role => role.name == defaultRole.name);
       return hasRole
     }).size}`, true)
-    // .addField("**Verified Member List:**", `${message.guild.members.filter((member,result) =>{
-    //   let defaultRole = member.guild.roles.find(r => r.name === assignRole);
-    //   let hasRole = member.roles.find(role => role.name == defaultRole.name);
-    //   return hasRole
-    //   }).map(member => member.user.username)}`, true)
     .addField("**Channels Count:**", `${message.guild.channels.size}`, true)
     .addField("**Role Count:**", `${message.guild.roles.size}`, true)
     .addField("**Role List:**", `${message.guild.roles.map(role=>role.name)}`, true)
     .setFooter(`${botTriggerCommand}`, bot.user.displayAvatarURL);
-    message.channel.send({embed});
+  await (message.channel.send({
+    embed
+  }));
+  let verifedMemberList = new Discord.RichEmbed()
+    .setColor("#5780cd")
+    .setTitle("Verifed Member List")
+    .setThumbnail(message.guild.iconURL)
+    .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
+    .addField("**Verified Member List:**", `${message.guild.members.filter((member,result) =>{
+      let defaultRole = member.guild.roles.find(r => r.name === assignRole);
+      let hasRole = member.roles.find(role => role.name == defaultRole.name);
+      return hasRole
+      }).map(member => member.user.username)}`, true)
+  await (message.channel.send({
+    verifedMemberList
+  }));
 }
 
 module.exports.config = {
-    name: "serverinfo",
-    description: "Get Server Info.",
-    usage:`${botTriggerCommand} serverinfo`
+  name: "serverinfo",
+  description: "Get Server Info.",
+  usage: `${botTriggerCommand} serverinfo`
 }
