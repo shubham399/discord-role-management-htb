3958ea96c2b3735530edba42ba78fac8195f315f
Shubham <shubham.399@gmail.com>
2019-11-16 13:13:56 +0530

    added exitHandler.js

--- services/exitHandler.js
+++ services/exitHandler.js
@@ -1,33 +1,37 @@
 'use strict'
+const botTriggerCommand = process.env.BOT_TRIGGER_COMMAND
+
 const logger = require('../log.js').logger
 
-function exitHandler(client,options) {
-  // const nclient = new Discord.Client();
-  // nclient.login(token);
-  try {
-    logger.info('Cleaning and Exiting')
-    if (process.env.NODE_ENV === 'uat') {
-      client.user.setActivity(`${botTriggerCommand} is unavailable`, {
-        type: 'Playing'
-      })
-      client.user.setStatus('offline').then(res => {
-        logger.info('Status Changed', res)
+function handle(client) {
+  return function exitHandler(options) {
+    try {
+      logger.info('Cleaning and Exiting')
+      if (process.env.NODE_ENV === 'uat') {
+        client.user.setActivity(`${botTriggerCommand} is unavailable`, {
+          type: 'Playing'
+        })
+        client.user.setStatus('offline').then(res => {
+          logger.info('Status Changed', res)
+          process.exit()
+        }).catch(err => {
+          logger.error('Cleanup Error', err)
+        })
+      } else {
         process.exit()
-      }).catch(err => {
-        logger.error('Cleanup Error', err)
-      })
-    } else {
+      }
+    } catch (e) {
+      logger.error(e)
       process.exit()
     }
-  } catch (e) {
-    logger.error(e)
-    process.exit()
   }
 }
 
 module.exports = (client) => {
   process.stdin.resume();
-  ['SIGTERM', 'SIGINT', 'uncaughtException'].map(signal => {
-    process.on(signal, exitHandler)
+  const exitHandle = handle(client);
+  // 'uncaughtException'
+  ['SIGTERM', 'SIGINT'].map(signal => {
+    process.on(signal, exitHandle)
   })
 }
