1e0f7f0dc6f52fd9c8e71084d8cf730d719f0332
Angad Gill <angad.gill@go-jek.com>
2019-11-16 13:19:38 +0530

    Add `config/colors.js`
    To map color names to hex codes for readability

--- config/colors.js
+++ config/colors.js
@@ -1,4 +1,13 @@
-module.exports = {
-  RED : 
-  BLUE : '#5780cd'
-}
+const COLORS =
+      {
+        'RED' : '#f14517',
+        'LIGHT_BLUE' : '#5780cd',
+        'YELLOW': '#f0d31a',
+        'CYAN_BLUE' :'#006ce5',
+        'LIME' : '#00E500',
+        'DUNE': '#514f48',
+        'VIVID_BLUE': '#1a85f0',
+        'DARK_RED': '#bc0000'
+      }
+
+module.exports = COLORS
