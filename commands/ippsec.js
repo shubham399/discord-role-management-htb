be1b144903bf5d2ecdf2adf1496edd01e31b57c7
Shubham <shubham.399@gmail.com>
2019-12-24 18:25:20 +0530

    fixed bug.

--- commands/ippsec.js
+++ commands/ippsec.js
@@ -6,11 +6,13 @@ const key = process.env.YOUTUBE_KEY;
 const axios = require("axios");
 const redis = require('../services/redis.js');
 const logger = require('../log.js').logger
+const R = require('ramda');
 
 const getBoxVideoLink = async (box) => {
   let response = await axios.get(constant.youtubeapi(boxname, key))
   let body = response.data;
-  let videoId = body.items[0].id.videoId;
+  let videoId = R.path(["items","0","id","videoId"],body); //body.items[0].id.videoId;
+  logger.verbose("Video Id from Response " + videoId);
   if (videoId) {
     let videoLink = `https://www.youtube.com/watch?v=${videoId}`
     await (redis.setex('IPPSEC_' + boxname, videoLink, 24 * 3600)) //cache it
@@ -30,7 +32,7 @@ module.exports.run = async (bot, message, args) => {
     let boxname = args[0].toLowerCase();
     const cachedResponse = await redis.get('IPPSEC_' + boxname);
     if (cachedResponse) {
-      logger.verbose("Sending URL from cache" + cachedResponse);
+      logger.info("Sending URL from cache" + cachedResponse);
       message.channel.send(cachedResponse)
     } else {
       let videoLink = await getBoxVideoLink(boxname);
