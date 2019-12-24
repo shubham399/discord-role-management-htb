db44434a0efea051d3c11eeb87028d68d9638724
Shubham <shubham.399@gmail.com>
2019-12-24 18:05:15 +0530

    cache the response.

--- commands/ippsec.js
+++ commands/ippsec.js
@@ -4,7 +4,7 @@ const constant = require('../constant.js')
 const botTriggerCommand = process.env.BOT_TRIGGER_COMMAND
 const key = process.env.YOUTUBE_KEY;
 const axios = require("axios");
-
+const redis = require('../services/redis.js');
 
 
 module.exports.run = async (bot, message, args) => {
@@ -13,11 +13,19 @@ module.exports.run = async (bot, message, args) => {
     if (args.length != 1) {
         message.channel.send("Please Provide a box name to search")
     } else {
-        let response = await axios.get(constant.youtubeapi(args[0], key))
+        let boxname = args[0].toLowerCase();
+        const cachedResponse  = await redis.get('IPPSEC_' + boxname);
+        if (cachedResponse)
+        {
+            message.channel.send(cachedResponse)
+        }
+        let response = await axios.get(constant.youtubeapi(boxname, key))
         let body = response.data;
         let videoId = body.items[0].id.videoId;
         if (videoId) {
-            message.channel.send(`https://www.youtube.com/watch?v=${videoId}`)
+            let videoLink = `https://www.youtube.com/watch?v=${videoId}`
+            await (redis.setex('IPPSEC_' + boxname, videoLink, 24 * 3600))
+            message.channel.send(videoLink);
         } else {
             message.channel.send("Please specify a valid box name.")
         }
