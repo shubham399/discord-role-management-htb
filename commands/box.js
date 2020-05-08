648176089e8a75cca0764612d3a41caa68de905b
Shubham <shubham.399@gmail.com>
2020-05-08 20:27:17 +0530

    Added box creation command in badgers.

---
+++ commands/box.js
@@ -0,0 +1,24 @@
+'use strict'
+const logger = require('../log.js').logger
+const Discord = require('discord.js')
+const actionLog = process.env.ACTION_LOG || 'action-log'
+const botTriggerCommand = process.env.BOT_TRIGGER_COMMAND
+
+module.exports.run = async (bot, message, args) => {
+  message.delete(2000)
+  let guild = message.guild;
+  let boxname = args[0].toLowerCase();
+  let category = guild.channels.find(c => c.name.toLowerCase().includes("boxes") && c.type == "category");
+  let hints = await guild.createChannel(`${boxname}-hints`,"text");
+  hints.setParent(category.id)
+  let discussion = await guild.createChannel(`${boxname}-discussion`,"text");
+  discussion.setParent(category.id)
+}
+
+module.exports.config = {
+  name: 'box',
+  description: 'Create a box discussion and hint channel.',
+  usage: `${botTriggerCommand} box <boxname>`,
+  minargs: 1,
+  minPermission: 'BAN_MEMBERS'
+}
